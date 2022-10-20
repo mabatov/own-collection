a8fb0c3e8a64d082a75cafd0646696d5e35f0799
Nikita <nikita-mabatov@mail.ru>
2022-10-20 21:10:44 +0300

    added set method added unit tests
    small refactoring

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-public class MyArrayList<T> implements Comparator {
+public class MyArrayList<T> implements Comparator<Object> {
 
     private final int INIT_SIZE = 16;
     private final int CUT_RATE = 4;
@@ -15,14 +15,38 @@ public class MyArrayList<T> implements Comparator {
     }
 
     public T get(int index) {
-        if(index >= pointer) {
+        if (index >= pointer) {
             throw new IndexOutOfBoundsException("Invalid index " + index + ". The size of current list is " + pointer);
         }
 
         return (T) array[index];
     }
 
+    public void set(int index, T item) {
+
+        if (index >= pointer) {
+            throw new IndexOutOfBoundsException("Invalid index " + index + ". The size of current list is " + pointer);
+        }
+
+        if (pointer == array.length - 1) {
+            resize(array.length * 2);
+        }
+
+        for (int i = pointer; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+
+        array[index] = item;
+
+        pointer++;
+    }
+
     public void remove(int index) {
+
+        if (index >= pointer) {
+            throw new IndexOutOfBoundsException("Invalid index " + index + ". The size of current list is " + pointer);
+        }
+
         for (int i = index; i < pointer; i++) {
             array[i] = array[i + 1];
         }
@@ -48,10 +72,10 @@ public class MyArrayList<T> implements Comparator {
         for (int i = 0; i < pointer - 1; i++) {
 
             for (int j = 0; j < pointer - i - 1; j++) {
-                if (compare(array[j],array[j+1]) > 0) {
+                if (compare(array[j], array[j + 1]) > 0) {
                     Object temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
 
@@ -61,7 +85,7 @@ public class MyArrayList<T> implements Comparator {
     @Override
     public int compare(Object o1, Object o2) {
 
-        if(o1 instanceof Number && o2 instanceof Number) {
+        if (o1 instanceof Number && o2 instanceof Number) {
             return (int) (((Number) o1).doubleValue() - ((Number) o2).doubleValue());
         }
 
